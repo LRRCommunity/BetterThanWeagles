8c2673e7a46f34730694f2cfe8da54e73fabf0e6
DarkMorford <DarkMorford@users.noreply.github.com>
2017-08-03 08:28:21 -0700

    Make jelly bean effect duration configurable.

--- src/main/java/net/darkmorford/btweagles/Config.java
+++ src/main/java/net/darkmorford/btweagles/Config.java
@@ -8,8 +8,10 @@ public class Config
 {
 	// Configuration categories
 	private static final String catGeneral = "general";
+	private static final String catJellyBeans = "jellybeans";
 
 	// Configuration values
+	public static int jellybean_duration_generic = 100;
 
 	public static void readConfig()
 	{
@@ -18,6 +20,7 @@ public class Config
 		{
 			cfg.load();
 			initGeneralConfig(cfg);
+			initJellyBeanConfig(cfg);
 		}
 		catch (Exception e)
 		{
@@ -35,5 +38,12 @@ public class Config
 	private static void initGeneralConfig(Configuration cfg)
 	{
 		cfg.addCustomCategoryComment(catGeneral, "General configuration");
+	}
+
+	private static void initJellyBeanConfig(Configuration cfg)
+	{
+		cfg.addCustomCategoryComment(catJellyBeans, "Jelly bean effect duration (in ticks)");
+
+		jellybean_duration_generic = cfg.getInt("duration_generic", catJellyBeans, jellybean_duration_generic, 0, 600, "Duration for all jelly beans");
 	}
 }
