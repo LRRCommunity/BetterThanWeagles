f9bdb5d6025a17ae55f4db6409f38dac697abfb5
DarkMorford <DarkMorford@users.noreply.github.com>
2017-08-08 14:44:37 -0700

    Possible fix for Note particles not appearing.

--- src/main/java/net/darkmorford/btweagles/eventhandler/EventHandlerNoteBlock.java
+++ src/main/java/net/darkmorford/btweagles/eventhandler/EventHandlerNoteBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.block.Block;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.event.world.NoteBlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -26,7 +27,7 @@ public class EventHandlerNoteBlock
 
 			BlockPos pos = event.getPos();
 			event.getWorld().playSound(null, pos, ModSounds.airhorn, SoundCategory.RECORDS, 3.0F, pitch);
-			event.getWorld().spawnParticle(EnumParticleTypes.NOTE, (double)pos.getX() + 0.5D, (double)pos.getY() + 1.2D, (double)pos.getZ() + 0.5D, (double)note / 24.0D, 0.0D, 0.0D);
+			((WorldServer)event.getWorld()).spawnParticle(EnumParticleTypes.NOTE, false, (double)pos.getX() + 0.5D, (double)pos.getY() + 1.2D, (double)pos.getZ() + 0.5D, 1, 0.0, 0.0, 0.0, (double)note / 24.0D);
 
 			if (event.isCancelable())
 			{
