77fe0e3c7837b0caa1a430886fb448d987895a41
DarkMorford <DarkMorford@users.noreply.github.com>
2017-08-10 12:06:18 -0700

    Torg come from jungle. Trade meat for jungle tree.

--- src/main/java/net/darkmorford/btweagles/villager/ModVillagers.java
+++ src/main/java/net/darkmorford/btweagles/villager/ModVillagers.java
@@ -2,6 +2,7 @@ package net.darkmorford.btweagles.villager;
 
 import net.darkmorford.btweagles.item.ModItems;
 import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.VillagerRegistry;
@@ -20,5 +21,7 @@ public class ModVillagers
 			"btweagles:textures/entity/villager/torg.png",
 			"minecraft:textures/entity/villager/zombie_villager.png");
 
-	protected static final VillagerRegistry.VillagerCareer careerTorg = new VillagerRegistry.VillagerCareer(professionTorg, "careerTorg");
+	protected static final VillagerRegistry.VillagerCareer careerTorg = new VillagerRegistry.VillagerCareer(professionTorg, "careerTorg")
+			.addTrade(1, new VillagerTradeItemForItem(new ItemStack(Items.BEEF), new EntityVillager.PriceInfo(1, 2),
+					new ItemStack(Blocks.SAPLING, 1, 3), new EntityVillager.PriceInfo(1, 2)));
 }
