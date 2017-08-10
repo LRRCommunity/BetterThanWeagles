e2ee5ccce3c86808c488b12b593c1df262afc1bb
DarkMorford <DarkMorford@users.noreply.github.com>
2017-08-10 11:23:56 -0700

    Add a tooltip to Meme Ore indicating its WIP status.

--- src/main/java/net/darkmorford/btweagles/block/BlockMemeOre.java
+++ src/main/java/net/darkmorford/btweagles/block/BlockMemeOre.java
@@ -4,10 +4,16 @@ import net.darkmorford.btweagles.BetterThanWeagles;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
+import java.util.List;
 
 public class BlockMemeOre extends Block
 {
@@ -28,5 +34,13 @@ public class BlockMemeOre extends Block
 	public void initModel()
 	{
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, new ModelResourceLocation(getRegistryName(), "inventory"));
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, World player, List<String> tooltip, ITooltipFlag advanced)
+	{
+		String tipString = I18n.format("tile.meme_ore.tooltip");
+		tooltip.add(tipString);
 	}
 }
