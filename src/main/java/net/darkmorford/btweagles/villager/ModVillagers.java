230c90b0a402cfcde71631b8666b5e4f4cc9af27
DarkMorford <DarkMorford@users.noreply.github.com>
2017-08-11 08:51:57 -0700

    Give Torg more trade options.

--- src/main/java/net/darkmorford/btweagles/villager/ModVillagers.java
+++ src/main/java/net/darkmorford/btweagles/villager/ModVillagers.java
@@ -2,11 +2,13 @@ package net.darkmorford.btweagles.villager;
 
 import com.pam.harvestcraft.item.ItemRegistry;
 import net.darkmorford.btweagles.item.ModItems;
+import net.minecraft.block.Block;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.registry.VillagerRegistry;
 
@@ -32,6 +34,16 @@ public class ModVillagers
 		careerTorg.addTrade(1, new VillagerTradeItemForItem(new ItemStack(Items.BEEF), new EntityVillager.PriceInfo(1, 3),
 				new ItemStack(Blocks.SAPLING, 1, 3), new EntityVillager.PriceInfo(1, 3)));
 
+		careerTorg.addTrade(1, new VillagerTradeItemForItem(new ItemStack(Items.LEATHER), new EntityVillager.PriceInfo(2, 4),
+				new ItemStack(Items.MELON_SEEDS), new EntityVillager.PriceInfo(4, 6)));
+
+		Block net = Block.REGISTRY.getObject(new ResourceLocation("actuallyadditions", "block_fishing_net"));
+		if (net != Blocks.AIR)
+		{
+			careerTorg.addTrade(2, new VillagerTradeItemForItem(new ItemStack(net), new EntityVillager.PriceInfo(3, 6),
+					new ItemStack(Items.DYE, 1, 3), new EntityVillager.PriceInfo(3, 6)));
+		}
+
 		NBTTagCompound parrotTag = new NBTTagCompound();
 		parrotTag.setString("id", "minecraft:parrot");
 		ItemStack parrotEgg = new ItemStack(Items.SPAWN_EGG, 1);
@@ -39,7 +51,7 @@ public class ModVillagers
 
 		if (Loader.isModLoaded("harvestcraft"))
 		{
-			careerTorg.addTrade(2, new VillagerTradeItemForItem(new ItemStack(ItemRegistry.honeycombItem), new EntityVillager.PriceInfo(30, 40),
+			careerTorg.addTrade(3, new VillagerTradeItemForItem(new ItemStack(ItemRegistry.honeycombItem), new EntityVillager.PriceInfo(30, 40),
 					parrotEgg, new EntityVillager.PriceInfo(1, 1)));
 		}
 	}
