e46235a48d9c9200a41788a1cef4ac5d061cdb38
DarkMorford <DarkMorford@users.noreply.github.com>
2017-08-15 11:14:36 -0700

    Use NBT to determine Simic Slaw's food properties.

--- src/main/java/net/darkmorford/btweagles/item/ItemSimicSlaw.java
+++ src/main/java/net/darkmorford/btweagles/item/ItemSimicSlaw.java
@@ -3,6 +3,7 @@ package net.darkmorford.btweagles.item;
 import net.darkmorford.btweagles.BetterThanWeagles;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -16,6 +17,45 @@ public class ItemSimicSlaw extends ItemFood
 		setRegistryName("simic_slaw");
 		setUnlocalizedName("simic_slaw");
 		setCreativeTab(BetterThanWeagles.tabBTWeagles);
+	}
+
+	@Override
+	public int getMaxItemUseDuration(ItemStack stack)
+	{
+		if (stack.hasTagCompound() && stack.getTagCompound().hasKey("speed"))
+		{
+			return stack.getTagCompound().getByte("speed");
+		}
+		else
+		{
+			return super.getMaxItemUseDuration(stack);
+		}
+	}
+
+	@Override
+	public int getHealAmount(ItemStack stack)
+	{
+		if (stack.hasTagCompound() && stack.getTagCompound().hasKey("hunger"))
+		{
+			return stack.getTagCompound().getByte("hunger");
+		}
+		else
+		{
+			return super.getHealAmount(stack);
+		}
+	}
+
+	@Override
+	public float getSaturationModifier(ItemStack stack)
+	{
+		if (stack.hasTagCompound() && stack.getTagCompound().hasKey("saturation"))
+		{
+			return stack.getTagCompound().getFloat("saturation");
+		}
+		else
+		{
+			return super.getSaturationModifier(stack);
+		}
 	}
 
 	@SideOnly(Side.CLIENT)
