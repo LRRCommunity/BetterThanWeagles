8653ec2194fdf99dbda5b957c2ff965fe183cebd
DarkMorford <DarkMorford@users.noreply.github.com>
2017-08-23 12:48:41 -0700

    Add spawn conditions.

--- src/main/java/net/darkmorford/btweagles/entity/ModEntities.java
+++ src/main/java/net/darkmorford/btweagles/entity/ModEntities.java
@@ -2,6 +2,8 @@ package net.darkmorford.btweagles.entity;
 
 import net.darkmorford.btweagles.BetterThanWeagles;
 import net.darkmorford.btweagles.entity.render.RenderSteveBeej;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.init.Biomes;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
@@ -14,7 +16,9 @@ public class ModEntities
 	public static void init()
 	{
 		int entityId = 1;
-		EntityRegistry.registerModEntity(new ResourceLocation("btweagles:stevebeej"), EntitySteveBeej.class, "SteveBeej", entityId++, BetterThanWeagles.instance, 64, 3, true, 0x996600, 0x00FF00);
+		EntityRegistry.registerModEntity(new ResourceLocation("btweagles:stevebeej"), EntitySteveBeej.class, "SteveBeej", entityId++, BetterThanWeagles.instance, 64, 3, true, 0xD1A288, 0x00CCCC);
+
+		EntityRegistry.addSpawn(EntitySteveBeej.class, 100, 1, 1, EnumCreatureType.MONSTER, Biomes.PLAINS);
 
 		LootTableList.register(EntitySteveBeej.LOOT_TABLE);
 	}
