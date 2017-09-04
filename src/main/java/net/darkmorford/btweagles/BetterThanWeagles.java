3ec6057e526a1bd66cdcac2160807593b09a7e80
DarkMorford <DarkMorford@users.noreply.github.com>
2017-09-04 14:59:46 -0700

    Bump version for release.

--- src/main/java/net/darkmorford/btweagles/BetterThanWeagles.java
+++ src/main/java/net/darkmorford/btweagles/BetterThanWeagles.java
@@ -29,7 +29,7 @@ public class BetterThanWeagles
 {
 	public static final String MODID = "btweagles";
 	public static final String MODNAME = "BetterThanWeagles";
-	public static final String VERSION = "1.12-1.0.2.0";
+	public static final String VERSION = "1.12-1.0.3.0";
 
 	@Mod.Instance
 	public static BetterThanWeagles instance;
