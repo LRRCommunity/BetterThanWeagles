b96230d29c801011c429a3f1bf382b52278101e6
DarkMorford <DarkMorford@users.noreply.github.com>
2017-10-13 16:45:28 -0700

    Remap "Every OS Sucks" to "Matt Wiggins".

--- src/main/java/net/darkmorford/btweagles/eventhandler/EventHandlerMappings.java
+++ src/main/java/net/darkmorford/btweagles/eventhandler/EventHandlerMappings.java
@@ -1,7 +1,10 @@
 package net.darkmorford.btweagles.eventhandler;
 
 import net.darkmorford.btweagles.BetterThanWeagles;
+import net.darkmorford.btweagles.item.ModItems;
+import net.darkmorford.btweagles.sound.ModSounds;
 import net.minecraft.item.Item;
+import net.minecraft.util.SoundEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -16,7 +19,34 @@ public class EventHandlerMappings
 
 		for (RegistryEvent.MissingMappings.Mapping<Item> mapping : event.getMappings())
 		{
-			BetterThanWeagles.logger.info("Item mapping for " + mapping.key + " is missing, taking default action.");
+			switch (mapping.key.toString())
+			{
+				case "btweagles:os_music":
+					BetterThanWeagles.logger.info("Item mapping for Every OS Sucks is missing, replacing it...");
+					mapping.remap(ModItems.matt_music);
+					break;
+				default:
+					BetterThanWeagles.logger.info("Item mapping for " + mapping.key + " is missing, taking default action.");
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public static void missingSoundMapping(RegistryEvent.MissingMappings<SoundEvent> event)
+	{
+		BetterThanWeagles.logger.warn("Caught missing sound mappings! Attempting to repair them...");
+
+		for (RegistryEvent.MissingMappings.Mapping<SoundEvent> mapping : event.getMappings())
+		{
+			switch (mapping.key.toString())
+			{
+				case "btweagles:os_music":
+					BetterThanWeagles.logger.info("Sound mapping for Every OS Sucks is missing, replacing it...");
+					mapping.remap(ModSounds.matt_music);
+					break;
+				default:
+					BetterThanWeagles.logger.info("Sound mapping for " + mapping.key + " is missing, taking default action.");
+			}
 		}
 	}
 }
